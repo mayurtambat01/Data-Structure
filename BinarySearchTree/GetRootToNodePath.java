01e1ea160716c98e7e39f880f0a9cea5c377b5c4
mayurtambat01 <mayurtambat01@gmail.com>
2024-07-29 13:06:17 +0530

    Add FindPath file

---
+++ BinarySearchTree/GetRootToNodePath.java
@@ -0,0 +1,55 @@
+/*Given a BST and an integer k. Find and return the path from the node 
+with data k and root (if a node with data k is present in given BST) 
+in a list. Return empty list otherwise.
+
+Note: Assume that BST contains all unique elements.
+*/
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GetRootToNodePath {
+
+    public static BinarySearchTree<Integer> takeInput(Scanner s){
+        System.out.println("Enter next Node");
+        int rootData = s.nextInt();
+        BinarySearchTree<Integer> root = new BinarySearchTree<Integer>(rootData);
+        if (root.data == -1) {
+            return null;
+        }
+        root.left = takeInput(s);
+        root.right = takeInput(s);
+
+        return root;
+    }
+    public static ArrayList<Integer> findPath(BinarySearchTree<Integer> root,int data){
+        if (root==null) {
+            return null;
+        }
+        if (root.data==data) {
+            ArrayList<Integer> output = new ArrayList<>();
+            output.add(root.data);
+            return output;
+        }
+        ArrayList<Integer> leftOutput = findPath(root.left, data);
+        if (leftOutput != null) {
+            leftOutput.add(root.data);
+            return leftOutput;
+        }
+
+        ArrayList<Integer> rightOutput = findPath(root.right, data);
+        if (rightOutput != null) {
+            rightOutput.add(root.data);
+            return rightOutput;
+        }else{
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        BinarySearchTree<Integer> root = takeInput(s);
+        ArrayList<Integer> rootData = findPath(root, 4);
+        System.out.println(rootData);        
+    }
+}
